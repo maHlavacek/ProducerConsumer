f874fb57bbe63f3803caefc93570fd636620b942
maHlavacek <martin_hlavacek@icloud.com>
2019-01-05 08:58:50 +0100

    Restart

--- ProducerConsumer.Core/Consumer.cs
+++ ProducerConsumer.Core/Consumer.cs
@@ -6,12 +6,6 @@ namespace ProducerConsumer.Core
     public class Consumer
     {
         #region Fields
-
-        private Task _currentTask;
-        private int _maxDuration;
-        private int _minDuration;
-        private int _create
-
         #endregion
     }
 }
--- ProducerConsumer.Core/Task.cs
+++ ProducerConsumer.Core/Task.cs
@@ -5,11 +5,6 @@ namespace ProducerConsumer.Core
     public class Task
     {
         #region Properties
-
-        public int BeginConsuptionTime { get; set; }
-        public int CreationTime { get; set; }
-        public int TaskNumber { get; set; }
-
         #endregion
     }
 }
