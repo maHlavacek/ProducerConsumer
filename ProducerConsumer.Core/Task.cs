59f22843088337a5dfdaaaff64f247928834bda3
maHlavacek <martin_hlavacek@icloud.com>
2019-01-05 09:17:45 +0100

    TaskImplementation

--- ProducerConsumer.Core/Task.cs
+++ ProducerConsumer.Core/Task.cs
@@ -5,6 +5,23 @@ namespace ProducerConsumer.Core
     public class Task
     {
         #region Properties
+        public DateTime BeginConsuptionTime { get; private set; }
+        private int CreationTime { get; set; }
+        private int TaskNumber { get; set; }
+        #endregion
+
+        #region EventHandler
+        public event EventHandler<Task> LogTask;
+        #endregion
+
+        #region Constructor
+        public Task()
+        {
+            BeginConsuptionTime = DateTime.Now;
+        }
+        #endregion
+
+        #region Methods
         #endregion
     }
 }
